7561542cab7cc04623b5d993ca0f886ff8793afe
wasaequreshi <wasaequreshi@gmail.com>
2020-07-04 12:13:17 -0700

    add test

--- test_cli_vault.py
+++ test_cli_vault.py
@@ -1,6 +1,10 @@
 import unittest
 from cli_vault import cli_vault
 from pathlib import Path
+import shutil
+import os
+from types import SimpleNamespace
+import json
 
 class TestCliVault(unittest.TestCase):
 
@@ -8,7 +12,42 @@ class TestCliVault(unittest.TestCase):
         self.sv = cli_vault(True)
     
     def test_add(self):
-        pass
+        args = SimpleNamespace(command="command",description="description",tags="tags")
+        self.sv.add(args)
+        
+        command_data = {}
+        with open(os.path.join('.secure_vault', 'commands.json')) as json_file:
+            command_data = json.load(json_file)
+            data = command_data['data']
+            
+            self.assertTrue(len(data) == 1)
+            self.assertTrue(data[0]['command'] == "command" and data[0]['description'] == "description" and data[0]['tags'] == ["tags"])
+
+        args = SimpleNamespace(command="command1",description=None,tags="tags1")
+        self.sv.add(args)
+
+        command_data = {}
+        with open(os.path.join('.secure_vault', 'commands.json')) as json_file:
+            command_data = json.load(json_file)
+            data = command_data['data']
+            
+            self.assertTrue(len(data) == 2)
+            self.assertTrue(data[1]['command'] == "command1" and data[1]['description'] == "" and data[1]['tags'] == ["tags1"])
+
+        args = SimpleNamespace(command="command2",description=None,tags="tags1,tags2")
+        self.sv.add(args)
+
+        command_data = {}
+        with open(os.path.join('.secure_vault', 'commands.json')) as json_file:
+            command_data = json.load(json_file)
+            data = command_data['data']
+            
+            self.assertTrue(len(data) == 3)
+            self.assertTrue(data[2]['command'] == "command2" and data[2]['description'] == "" and data[2]['tags'] == ["tags1", "tags2"])
+            
+    def tearDown(self):
+        dir_path = os.path.join(".secure_vault")
+        shutil.rmtree(dir_path)
 
 if __name__ == '__main__':
     unittest.main()
