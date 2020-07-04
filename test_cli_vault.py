1db56d8c2a4be904f9150c5e8168940d3578742e
wasaequreshi <wasaequreshi@gmail.com>
2020-07-04 12:22:40 -0700

    Tests for add, delete, and update

--- test_cli_vault.py
+++ test_cli_vault.py
@@ -44,7 +44,62 @@ class TestCliVault(unittest.TestCase):
             
             self.assertTrue(len(data) == 3)
             self.assertTrue(data[2]['command'] == "command2" and data[2]['description'] == "" and data[2]['tags'] == ["tags1", "tags2"])
-            
+    
+    def test_delete(self):
+        
+        args = SimpleNamespace(command="command",description="description",tags="tags")
+        self.sv.add(args)
+        args = SimpleNamespace(command="command1",description=None,tags="tags1")
+        self.sv.add(args)
+        args = SimpleNamespace(command="command2",description=None,tags="tags1,tags2")
+        self.sv.add(args)
+
+        ids = []
+
+        with open(os.path.join('.secure_vault', 'commands.json')) as json_file:
+            command_data = json.load(json_file)
+            data = command_data['data']
+            self.assertTrue(len(data) == 3)
+
+            for d in data:
+                ids.append(d['id'])
+
+        args = SimpleNamespace(command_id=ids[0])
+        self.sv.delete(args)
+
+        with open(os.path.join('.secure_vault', 'commands.json')) as json_file:
+            command_data = json.load(json_file)
+            data = command_data['data']
+            self.assertTrue(len(data) == 2)
+
+    def test_update(self):
+
+        args = SimpleNamespace(command="command",description="description",tags="tags")
+        self.sv.add(args)
+        args = SimpleNamespace(command="command1",description=None,tags="tags1")
+        self.sv.add(args)
+        args = SimpleNamespace(command="command2",description=None,tags="tags1,tags2")
+        self.sv.add(args)
+
+        ids = []
+        
+        with open(os.path.join('.secure_vault', 'commands.json')) as json_file:
+            command_data = json.load(json_file)
+            data = command_data['data']
+            self.assertTrue(len(data) == 3)
+
+            for d in data:
+                ids.append(d['id'])
+
+        args = SimpleNamespace(command_id=ids[0], command="command19",description=None,tags=None)
+        self.sv.update(args)
+        
+        with open(os.path.join('.secure_vault', 'commands.json')) as json_file:
+            command_data = json.load(json_file)
+            data = command_data['data']
+            self.assertTrue(len(data) == 3)
+            self.assertTrue(data[0]['command'] == "command19" and data[0]['description'] == "description" and data[0]['tags'] == ["tags"])
+
     def tearDown(self):
         dir_path = os.path.join(".secure_vault")
         shutil.rmtree(dir_path)
