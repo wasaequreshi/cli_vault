8fe68a38226344886b8929ea3244eafb44d6d52f
wasaequreshi <wasaequreshi@gmail.com>
2020-07-11 01:21:11 -0700

    removing output for users from tests

--- tests/test_cli_vault.py
+++ tests/test_cli_vault.py
@@ -15,7 +15,7 @@ class TestCliVault(unittest.TestCase):
     
     def test_add(self):
         args = SimpleNamespace(cli_note="cli_note",description="description",tags="tags")
-        self.sv.add(args)
+        self.sv._add(args)
         
         cli_note_data = {}
         with open(os.path.join('.secure_vault', 'cli_notes.json')) as json_file:
@@ -26,7 +26,7 @@ class TestCliVault(unittest.TestCase):
             self.assertTrue(data[0]['cli_note'] == ["cli_note"] and data[0]['description'] == ["description"] and data[0]['tags'] == ["tags"])
 
         args = SimpleNamespace(cli_note="cli_note1",description=None,tags="tags1")
-        self.sv.add(args)
+        self.sv._add(args)
 
         cli_note_data = {}
         with open(os.path.join('.secure_vault', 'cli_notes.json')) as json_file:
@@ -36,7 +36,7 @@ class TestCliVault(unittest.TestCase):
             self.assertTrue(data[1]['cli_note'] == ["cli_note1"] and data[1]['description'] == [""] and data[1]['tags'] == ["tags1"])
 
         args = SimpleNamespace(cli_note="cli_note2",description=None,tags="tags1,tags2")
-        self.sv.add(args)
+        self.sv._add(args)
 
         cli_note_data = {}
         with open(os.path.join('.secure_vault', 'cli_notes.json')) as json_file:
@@ -49,11 +49,11 @@ class TestCliVault(unittest.TestCase):
     def test_delete(self):
         
         args = SimpleNamespace(cli_note="cli_note",description="description",tags="tags")
-        self.sv.add(args)
+        self.sv._add(args)
         args = SimpleNamespace(cli_note="cli_note1",description=None,tags="tags1")
-        self.sv.add(args)
+        self.sv._add(args)
         args = SimpleNamespace(cli_note="cli_note2",description=None,tags="tags1,tags2")
-        self.sv.add(args)
+        self.sv._add(args)
 
         ids = []
 
@@ -66,21 +66,21 @@ class TestCliVault(unittest.TestCase):
                 ids.append(d['id'])
 
         args = SimpleNamespace(cli_note_id=ids[0])
-        self.sv.delete(args)
+        self.sv._delete(args)
 
         with open(os.path.join('.secure_vault', 'cli_notes.json')) as json_file:
             cli_note_data = json.load(json_file)
             data = cli_note_data['data']
             self.assertTrue(len(data) == 2)
 
-    def test_update(self):
+    def test__update(self):
 
         args = SimpleNamespace(cli_note="cli_note",description="description",tags="tags")
-        self.sv.add(args)
+        self.sv._add(args)
         args = SimpleNamespace(cli_note="cli_note1",description=None,tags="tags1")
-        self.sv.add(args)
+        self.sv._add(args)
         args = SimpleNamespace(cli_note="cli_note2",description=None,tags="tags1,tags2")
-        self.sv.add(args)
+        self.sv._add(args)
 
         ids = []
         
@@ -93,7 +93,7 @@ class TestCliVault(unittest.TestCase):
                 ids.append(d['id'])
 
         args = SimpleNamespace(cli_note_id=ids[0], cli_note="cli_note19",description=None,tags=None)
-        self.sv.update(args)
+        self.sv._update(args)
         
         with open(os.path.join('.secure_vault', 'cli_notes.json')) as json_file:
             cli_note_data = json.load(json_file)
@@ -103,7 +103,7 @@ class TestCliVault(unittest.TestCase):
 
     def test_search(self):
         args = SimpleNamespace(cli_note="ssh -i my_private_key ubuntu@localhost",description="ssh into server with private key",tags="ssh,private key,unique")
-        self.sv.add(args)
+        self.sv._add(args)
 
         args = SimpleNamespace(text='into', a=False, c=False, d=False, t=False)
 
@@ -160,7 +160,7 @@ class TestCliVault(unittest.TestCase):
         self.assertTrue(len(results['data']) == 0)
 
         args = SimpleNamespace(cli_note="ssh -i my_private_key ubuntu@localhost",description="ssh into server with private key",tags="ssh,private key,unique")
-        self.sv.add(args)
+        self.sv._add(args)
         args = SimpleNamespace(cli_note="ssh -i my_private_key ubuntu@localhost",description="ssh into server with private key",tags="ssh,private key,unique")
 
         results = self.sv._list_cli_notes(None)
